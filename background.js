8481ca3e4c5a07fb9d8f5c94cafb8b912460d9e7
Matteo Noro <86775603+TheoMadCo@users.noreply.github.com>
2022-03-04 12:54:38 +0100

    Content Update (#2)
    * Content Update
    
    + Label values
    + Graphical enhancement
    
    * Silly fix
    
    * Metric Values
    
    * Silly fix
    
    * Metric colors fixed
    
    * h1 fix

--- background.js
+++ background.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     var con = document.getElementById("Extension-conSlider___")
     var res = document.getElementById("Extension-reset___")
 
+    var outhue = document.getElementById("huedisplay")
+    var outsat = document.getElementById("satdisplay")
+    var outbri = document.getElementById("bridisplay")
+    var outcon = document.getElementById("condisplay")
 
     hue.value = localStorage["hueValue"] || 0
     sat.value = localStorage["satValue"] || 100
@@ -16,6 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
         localStorage["satValue"] = sat.value
         localStorage["briValue"] = bri.value
         localStorage["conValue"] = con.value
+
+        outhue.innerHTML = hue.value + "&deg;"
+        outsat.innerHTML = sat.value + "&percnt;"
+        outbri.innerHTML = bri.value + "&percnt;"
+        outcon.innerHTML = con.value + "&percnt;"
 
         chrome.tabs.executeScript(null, {code: 
             "document.documentElement.style.setProperty('filter', 'hue-rotate("+hue.value+"deg) saturate("+sat.value+"%) contrast("+con.value+"%) brightness("+bri.value+"%)');"
